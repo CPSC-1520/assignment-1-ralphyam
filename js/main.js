0a3918aa8fd9b78977d56eab9216badb3f276b0e
ralphyam <ryammine1@nait.ca>
2024-10-09 11:01:17 -0600

    testing
    more testing

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Please refer to the "Required Tasks in the assignments PDF"
 
 // html for the add cheep create function
-/*
+
   <div class="col">
     <div class="card shadow-sm">
       <img class="bd-placeholder-img card-img-top" src="ALBUM IMAGE SELECTION HERE"/>
@@ -12,76 +12,4 @@
       </div>
     </div>
   </div>
-*/
-document.addEventListener('DOMContentLoaded', () => {
-  const albumForm = document.getElementById('album-form');
 
-
-  albumForm.addEventListener('submit', function (event) {
-    event.preventDefault(); 
-
-    const formElements = event.target.elements;
-    const albumTitle = formElements['album-title'].value.trim();
-    const albumDescription = formElements['album-description'].value.trim();
-    const albumArt = formElements['album-art'].value;
-
-  
-    clearValidationErrors(formElements);
-
-    const isValid = validateInputs(albumTitle, albumDescription, albumArt, formElements);
-
-    if (isValid) {
-      createAlbum(albumTitle, albumDescription, albumArt);
-
-    
-      albumForm.reset();
-    }
-  });
-
-
-  function clearValidationErrors(formElements) {
-    formElements['album-title'].classList.remove('is-invalid');
-    formElements['album-description'].classList.remove('is-invalid');
-    formElements['album-art'].classList.remove('is-invalid');
-  }
-
-  function validateInputs(title, description, art, formElements) {
-    let isValid = true;
-
-    if (title === '') {
-      formElements['album-title'].classList.add('is-invalid');
-      isValid = false;
-    }
-
-    if (description === '') {
-      formElements['album-description'].classList.add('is-invalid');
-      isValid = false;
-    }
-
-    if (art === '') {
-      formElements['album-art'].classList.add('is-invalid');
-      isValid = false;
-    }
-
-    return isValid;
-  }
-
-  function createAlbum(title, description, art) {
-    const albumList = document.getElementById('all-albums-list');
-
-    const albumCard = document.createElement('div');
-    albumCard.classList.add('col');
-
-    albumCard.innerHTML = `
-      <div class="card shadow-sm">
-        <img src="img/${art}" class="bd-placeholder-img card-img-top" width="100%" height="225" alt="${title}">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class="card-text">${description}</p>
-        </div>
-      </div>
-    `;
-
-    albumList.appendChild(albumCard);
-  }
-});
